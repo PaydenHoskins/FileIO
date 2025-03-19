502b606fcb39d17bc5dea03254dbd5602c1eef77
Payden Hoskins <paydenhoskins@gmail.com>
2025-03-19 08:35:08 -0600

    Added a file append

--- FileIO/FileIO.vb
+++ FileIO/FileIO.vb
@@ -12,12 +12,27 @@ Module FileIO
     Sub Main()
         WriteToFile()
     End Sub
-
+    ''' <summary>
+    ''' Writes a predetermined string to the file that is being called. Replaces any previous lines before it.
+    ''' </summary>
     Sub WriteToFile()
 
         FileOpen(1, "MyCoolFile.txt", OpenMode.Output)
 
-        Write(1, "Hello Moto.")
+        Write(1, "Follow The White Rabbit...")
+
+        FileClose(1)
+
+    End Sub
+    ''' <summary>
+    ''' Writes mutiple lines without deleating previous data in the .txt file.
+    ''' </summary>
+    Sub AppendAFile()
+
+        FileOpen(1, "MyCoolFile.txt", OpenMode.Append)
+        For i = 1 To 10
+            Write(1, "You are my sunshine, my only sunshine.")
+        Next
 
         FileClose(1)
 
